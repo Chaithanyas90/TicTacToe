e6f3574d7b7135e09d0e94c66b41bdf44d800013
CHAITHANYA S <chaithanya.s@aon.com>
2025-03-14 18:38:36 +0530

    Updated Save

--- NumericTicTacToe/SaveManager.cs
+++ NumericTicTacToe/SaveManager.cs
@@ -12,20 +12,25 @@ namespace NumericTicTacToe
         public void SaveGame(List<Move> moves)
         {
             Console.Write("Enter the file path to save the game: ");
-            string filePath = Console.ReadLine();
+            string folderPath = Console.ReadLine();
 
-            if (!IsValidFilePath(filePath))
+            if (!IsValidFilePath(folderPath))//C:\Users\A0821423\Downloads\Keerthan\File.txt
             {
                 Console.WriteLine("Invalid file path. Please enter a valid path.");
                 return;
             }
 
+            string fileName = "Numeric_Tic_Tac_Toe_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
+            string filePath = Path.Combine(folderPath, fileName);
             using (StreamWriter writer = new StreamWriter(filePath))
             {
+                var player = string.Empty;
                 foreach (var move in moves)
                 {
+                    player = move.Player.Name;
                     writer.WriteLine($"{move.Player.Name},{move.Position.X},{move.Position.Y},{move.Value},{move.Timestamp}");
                 }
+                writer.WriteLine($"{player} has won!");
             }
             Console.WriteLine("Game successfully saved.");
         }
